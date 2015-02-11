90a4798afc1495ea6eea773beb31ff8960673e91
Vytautas Astrauskas <vastrauskas@gmail.com>
2015-02-11 21:42:05 +0100

    Refactoring silicon invocation script.

---
+++ bin/settings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DOCKER_COMMAND="docker run --rm -ti"
+export DOCKER_HOME=/home/developer
+export DOCKER_SOURCE_DIR="$DOCKER_HOME/source"
+export DOCKER_SBT="sbt -no-share"
+export DOCKER_IMGAGE="vakaras/viper-build:0.0.2"
