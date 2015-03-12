071c6de805eb1f991526a8a6932639eab59a49af
Vytautas Astrauskas <vastrauskas@gmail.com>
2015-03-12 21:34:35 +0100

    Bumped viper-build version.

--- bin/settings.sh
+++ bin/settings.sh
@@ -4,4 +4,4 @@ export DOCKER_COMMAND="docker run --rm -ti"
 export DOCKER_HOME=/home/developer
 export DOCKER_SOURCE_DIR="$DOCKER_HOME/source"
 export DOCKER_SBT="sbt -no-share"
-export DOCKER_IMGAGE="vakaras/viper-build:0.0.2"
+export DOCKER_IMGAGE="vakaras/viper-build:0.0.3"
