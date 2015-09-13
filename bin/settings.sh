54de6e200c00d0bc5ee88d5f578ce3c0e9031b7f
Vytautas Astrauskas <vastrauskas@gmail.com>
2015-09-13 18:02:54 +0200

    Bug fix: detected location of the script is incorrect when symlinks are used.

--- bin/settings.sh
+++ bin/settings.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-export BIN_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# https://bosker.wordpress.com/2012/02/12/bash-scripters-beware-of-the-cdpath/
+unset CDPATH
+
+# https://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+export BIN_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 export ROOT_DIR="${BIN_DIR}/.."
 export WORKSPACE_DIR="${ROOT_DIR}/workspace"
 export NAILGUN_BIN="${ROOT_DIR}/nailgun/ng"
